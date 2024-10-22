eb8e040aa4a6bb49e8e3ebb13079402a11fb1630
Shlomit Horn <shlomithorn@gmail.com>
2024-10-22 06:37:26 +0300

    Add createPdf func

--- countries-to-pdf.js
+++ countries-to-pdf.js
@@ -2,14 +2,6 @@ import fs from "fs"
 import PDFDocument from "pdfkit-table"
 import { utilService } from "./util.service.js"
 
-getCountries()
-
-function getCountries() {
-    const countries = utilService
-        .readJsonFile("countries.json")
-        .then( countriesArr => createPdf(countriesArr))
-}
-
 // init document
 let doc = new PDFDocument({ margin: 30, size: "A4" })
 
@@ -18,16 +10,41 @@ doc.pipe(fs.createWriteStream("./countries.pdf"))
 
 createPdf(doc).then(() => doc.end()) // close document
 
-function createPdf(data) {
-    const table = {
-        title: "Countries",
-        subtitle: "Sorted by name",
-        headers: ["Country", "Capital", "Population"],
-        rows: [
-            ['Donni', '22', '10'],
-            ['Ronni', '25', '9'],
-            ['Boris', '27', '10'],
-        ],
-    }
-    return doc.table(table, { columnsSize: [200, 100, 100] })
+function getCountries() {
+    return new Promise((resolve, reject) => {
+        utilService.readJsonFile("countries.json").then(countriesArr => {
+            if (!countriesArr) reject(err)
+            let sorted = countriesArr.sort((a, b) => {
+                const x = a.name.common
+                const y = b.name.common
+                return x < y ? -1 : x > y ? 1 : 0
+            })
+
+            resolve(sorted)
+        })
+    })
+}
+
+function createPdf() {
+    return new Promise((resolve, reject) => {
+        getCountries().then(countries => {
+            if (!countries) reject(err)
+            const table = {
+                title: "Countries",
+                subtitle: "Sorted by name",
+                headers: ["Country", "Capital", "Population"],
+                rows: [],
+            }
+
+            for (let i = 0; i < countries.length; i++) {
+                table.rows.push([
+                    countries[i].name.common,
+                    countries[i].capital,
+                    countries[i].population,
+                ])
+            }
+
+            resolve(doc.table(table, { columnsSize: [200, 100, 100] }))
+        })
+    })
 }
